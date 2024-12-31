8c2318e4e7c64c390aab80eca405110fe9d5f4ea
NagesoHaile <nageehail12@gmail.com>
2024-12-31 09:51:54 +0300

    initial commit.

---
+++ bin/init_pro.dart
@@ -0,0 +1,5 @@
+import 'package:init_pro/init_pro.dart' as init_pro;
+
+void main(List<String> arguments) {
+  print('Hello world: ${init_pro.calculate()}!');
+}
---
+++ test/init_pro_test.dart
@@ -0,0 +1,8 @@
+import 'package:init_pro/init_pro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
