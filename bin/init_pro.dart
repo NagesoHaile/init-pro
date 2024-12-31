ce06c1ab2a53218be1f3d9d96f089d59753a5fc6
NagesoHaile <nageehail12@gmail.com>
2024-12-31 10:33:48 +0300

    feat: init commands initial setup done.

--- bin/init_pro.dart
+++ bin/init_pro.dart
@@ -1,5 +1,51 @@
 import 'package:init_pro/init_pro.dart' as init_pro;
+import 'dart:io';
+import 'package:args/args.dart';
+
+import '../lib/commands/init_command.dart';
 
 void main(List<String> arguments) {
+  final parser = ArgParser()
+    ..addCommand('init')
+    ..addCommand('create')
+    ..addCommand('add');
+
+  try {
+    final results = parser.parse(arguments);
+    if (results.command == null) {
+      print('No command provided. Use --help for a list of commands');
+      exit(0);
+    }
+
+    switch (results.command!.name) {
+      case 'init':
+        _handleInitCommand(results.command!);
+        break;
+      case 'create':
+        _handleCreateCommand(results.command!);
+        break;
+      case 'add':
+        _handleAddCommand(results.command!);
+        break;
+      default:
+        print('Unknown command. Use --help for a list of commands');
+    }
+  } catch (e) {
+    print('Error: $e');
+    print('Use --help for usage instructions.');
+  }
   print('Hello world: ${init_pro.calculate()}!');
+}
+
+void _handleInitCommand(ArgResults args) {
+  print('Initializing project...');
+  initProject();
+}
+
+void _handleCreateCommand(ArgResults args) {
+  print('Creating feature...');
+}
+
+void _handleAddCommand(ArgResults args) {
+  print('Adding component...');
 }
---
+++ lib/commands/init_command.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+void initProject() {
+  final currentDir = Directory.current;
+
+  // check if pubspec.yaml exists
+  final pubspecFile = File('${currentDir.path}/pubspec.yaml');
+  if (!pubspecFile.existsSync()) {
+    print(
+        'Error: pubspec.yaml file not found. Please run this command inside a Flutter project.');
+    exit(1);
+  }
+
+  // check if lib directory exits
+  final libDir = Directory('${currentDir.path}/lib');
+  if (!libDir.existsSync()) {
+    print(
+        'Error: lib/ directory not found. Please run this command inside a Flutter project.');
+    exit(1);
+  }
+
+  // create a clean architecture folders
+  final dataDir = Directory('${libDir.path}/data');
+  final domainDir = Directory('${libDir.path}/domain');
+  final presentationDir = Directory('${libDir.path}/presentation');
+
+  dataDir.createSync(recursive: true);
+  domainDir.createSync(recursive: true);
+  presentationDir.createSync(recursive: true);
+
+  print('Clean architecture folder structure created');
+  print('  - lib/data');
+  print('  - lib/domain');
+  print('  - lib/presentation');
+
+  // Create Default Helper Files
+  final helperDir = Directory('${libDir.path}/helpers');
+  helperDir.createSync(recursive: true);
+
+  final apiHelper = File('${helperDir.path}/api_helper.dart');
+  apiHelper.writeAsString('''
+     Class ApiHelper {
+      // Your API helper methods here
+     }
+''');
+
+  print('Helper file created: lib/helpers/api_helper.dart');
+
+  final validators = File('${helperDir.path}/validators.dart');
+  validators.writeAsString('''
+    Class Validators {
+      static String? validateEmail(String email){
+        if(email.isEmpty) return 'Email is required.';
+         final regex = RegExp(r'^\\S+@\\S+\\.\\S+\$');
+         return regex.hasMatch(email) ? null : 'Invalid email format';
+      }
+    }
+''');
+
+  print('Helper file created: lib/helpers/validators.dart');
+
+  final configFile = File('${currentDir.path}/initpro_config.yaml');
+  configFile.writeAsString('''
+ # Configuration fro InitPro CLI Tool
+ state_management: bloc
+ use_themes:true
+''');
+
+  print('Configuration file created: initpro_config.yaml');
+}
