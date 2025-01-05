36c9f8ad593c6bfd9e484b72cce333b9a52e0fc3
NagesoHaile <nageehail12@gmail.com>
2025-01-05 13:10:03 +0300

    fix: removed un used codes.

--- bin/init_pro.dart
+++ bin/init_pro.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:init_pro/commands/init_command.dart';
 import 'package:init_pro/commands/create_feature_command.dart';
@@ -34,59 +32,4 @@ Examples:
       print('An unexpected error occurred: $error');
     }
   });
-  // final parser = ArgParser()
-  //   ..addCommand('init')
-  //   ..addCommand('create')
-  //   ..addCommand('add');
-
-  // try {
-  //   final results = parser.parse(arguments);
-  //   if (results.command == null) {
-  //     print('No command provided. Use --help for a list of commands');
-  //     exit(0);
-  //   }
-
-  //   switch (results.command!.name) {
-  //     case 'init':
-  //       _handleInitCommand(results.command!);
-  //       break;
-  //     case 'create':
-  //       _handleCreateCommand(results.command!);
-  //       break;
-  //     case 'add':
-  //       _handleAddCommand(results.command!);
-  //       break;
-  //     default:
-  //       print('Unknown command. Use --help for a list of commands');
-  //   }
-  // } catch (e) {
-  //   print('Error: $e');
-  //   print('Use --help for usage instructions.');
-  // }
-}
-
-void _handleInitCommand(ArgResults args) {
-  print('Initializing project...');
-  initProject();
-}
-
-// void _printHelpDoc(ArgResults args){
-//   print('''
-//    Usage: dart pub run init_pro [arguments...]
-//    \-\-help           Print this usage information
-//   ''');
-// }
-
-void _handleCreateCommand(ArgResults args) {
-  if (args.rest.isEmpty) {
-    print('Error: Please provide a feature name.');
-    return;
-  }
-  final featureName = args.rest[0];
-  print('Creating feature $featureName...');
-  createFeature(featureName);
-}
-
-void _handleAddCommand(ArgResults args) {
-  print('Adding component...');
 }
